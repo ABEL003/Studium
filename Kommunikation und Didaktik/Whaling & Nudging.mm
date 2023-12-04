08852541fd0bc23f4b8d1042a5d769db31e836ed
ABEL003 <92093965+ABEL003@users.noreply.github.com>
2023-12-04 21:58:40 +0100

    Kommunikation und Didaktik
    +Whaling und Nudging Mind Map

---
+++ Kommunikation und Didaktik/Whaling & Nudging.mm
@@ -0,0 +1,46 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1701721468295" ID="ID_178229246" MODIFIED="1701721811380" TEXT="Whaling &amp; Nudging">
+<node CREATED="1701721767632" ID="ID_1714218005" MODIFIED="1701721801478" POSITION="right" TEXT="Whaling">
+<node CREATED="1701721780237" ID="ID_102587688" MODIFIED="1701723247658" TEXT="CEO Fraud">
+<node CREATED="1701722695775" ID="ID_414574149" MODIFIED="1701723253813" TEXT="gef&#xe4;lschte E-Mails, die vorgeben, vom CEO zu stammen und Finanztransaktionen anfordern"/>
+</node>
+<node CREATED="1701721789257" ID="ID_1186958709" MODIFIED="1701723248186" TEXT="Identit&#xe4;tsdiebstahl">
+<node CREATED="1701722720824" ID="ID_167589491" MODIFIED="1701722727342" TEXT="T&#xe4;uschung von F&#xfc;hrungskr&#xe4;ften zur Offenlegung pers&#xf6;nlicher Informationen oder Anmeldeinformationen"/>
+</node>
+<node CREATED="1701721801478" ID="ID_1201699457" MODIFIED="1701723248581" TEXT="Lieferantenbetrug">
+<node CREATED="1701722730126" ID="ID_479828706" MODIFIED="1701722737796" TEXT="Manipulation von E-Mails an Finanzabteilungen, um Zahlungen an betr&#xfc;gerische Lieferanten zu veranlassen"/>
+</node>
+<node CREATED="1701722290157" FOLDED="true" ID="ID_320001793" MODIFIED="1701722754775" TEXT="Board-Mitglieder-Angriffe">
+<node CREATED="1701722750382" ID="ID_1098126173" MODIFIED="1701722752018" TEXT="Gezielte Phishing-Angriffe auf Vorstandsmitglieder, um sensible Unternehmensinformationen zu erhalten"/>
+</node>
+</node>
+<node CREATED="1701721813579" ID="ID_1863309407" MODIFIED="1701722639927" POSITION="left" TEXT="Nudging">
+<node CREATED="1701721835293" ID="ID_1001477306" MODIFIED="1701722641646" TEXT="positive">
+<node CREATED="1701721855810" ID="ID_1320446596" MODIFIED="1701723246560" TEXT="Gesundheits-Apps">
+<node CREATED="1701721871608" ID="ID_1729581520" MODIFIED="1701721887017" TEXT="Anzeige von gesunden Verhaltensweisen oder Ern&#xe4;hrungstipps, um Benutzer zu ges&#xfc;nderen Entscheidungen zu beeinflussen"/>
+</node>
+<node CREATED="1701722488750" ID="ID_451287359" MODIFIED="1701723246016" TEXT="Umweltbewusstsein">
+<node CREATED="1701722506073" ID="ID_1403077683" MODIFIED="1701722508260" TEXT="Hervorhebung umweltfreundlicher Optionen beim Online-Shopping, um nachhaltige Entscheidungen zu f&#xf6;rdern"/>
+</node>
+<node CREATED="1701722520060" ID="ID_1591485737" MODIFIED="1701723245595" TEXT="Energieeinsparung">
+<node CREATED="1701722528687" ID="ID_345307349" MODIFIED="1701722530717" TEXT="Anzeige von Vergleichsdaten im Haushalt, um Benutzer zum Energiesparen zu motivieren"/>
+</node>
+</node>
+<node CREATED="1701721849112" ID="ID_663585185" MODIFIED="1701722656326" TEXT="negative">
+<node CREATED="1701721917439" ID="ID_1385903763" MODIFIED="1701723245147" TEXT="Online-Shopping">
+<node CREATED="1701721923757" ID="ID_610978177" MODIFIED="1701721928875" TEXT="Countdown-Timer oder begrenzte Verf&#xfc;gbarkeit, um Impulsk&#xe4;ufe zu f&#xf6;rdern."/>
+</node>
+</node>
+<node CREATED="1701721942435" ID="ID_1214533661" MODIFIED="1701723244201" TEXT="Social Media">
+<node CREATED="1701721961234" ID="ID_196557421" MODIFIED="1701721964632" TEXT="Benachrichtigungen &#xfc;ber Aktivit&#xe4;ten von Freunden oder Belohnungen f&#xfc;r regelm&#xe4;&#xdf;ige Nutzung, um die App-Nutzung zu steigern."/>
+</node>
+<node CREATED="1701722597890" ID="ID_1341500935" MODIFIED="1701723243699" TEXT="Cookie-Hinweise">
+<node CREATED="1701722609410" ID="ID_31481988" MODIFIED="1701722616888" TEXT="Pop-up-Benachrichtigungen, die die Zustimmung zur Verwendung von Cookies f&#xf6;rdern"/>
+</node>
+<node CREATED="1701722664732" ID="ID_1603034805" MODIFIED="1701723243209" TEXT="App-Entwicklung">
+<node CREATED="1701722675843" ID="ID_538152849" MODIFIED="1701722677392" TEXT="Nutzung von Gamification-Elementen, um Benutzer zur regelm&#xe4;&#xdf;igen Nutzung von Apps zu motivieren"/>
+</node>
+</node>
+</node>
+</map>
