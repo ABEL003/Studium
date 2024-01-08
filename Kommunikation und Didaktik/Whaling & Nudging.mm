1377d3531d345615c27fea5f6c5b53f2bdf58dbb
ABEL003 <92093965+ABEL003@users.noreply.github.com>
2024-01-08 08:15:52 +0100

    whaling und nudging + obsidian setup

--- Kommunikation und Didaktik/Whaling & Nudging.mm
+++ Kommunikation und Didaktik/Whaling & Nudging.mm
@@ -1,45 +1,83 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1701721468295" ID="ID_178229246" MODIFIED="1701852412593" TEXT="Whaling &amp; Nudging">
-<node CREATED="1701721767632" ID="ID_1714218005" MODIFIED="1701721801478" POSITION="right" TEXT="Whaling">
-<node CREATED="1701721780237" ID="ID_102587688" MODIFIED="1701723247658" TEXT="CEO Fraud">
-<node CREATED="1701722695775" ID="ID_414574149" MODIFIED="1701723253813" TEXT="gef&#xe4;lschte E-Mails, die vorgeben, vom CEO zu stammen und Finanztransaktionen anfordern"/>
-</node>
-<node CREATED="1701721789257" ID="ID_1186958709" MODIFIED="1701723248186" TEXT="Identit&#xe4;tsdiebstahl">
-<node CREATED="1701722720824" ID="ID_167589491" MODIFIED="1701722727342" TEXT="T&#xe4;uschung von F&#xfc;hrungskr&#xe4;ften zur Offenlegung pers&#xf6;nlicher Informationen oder Anmeldeinformationen"/>
-</node>
-<node CREATED="1701721801478" ID="ID_1201699457" MODIFIED="1701723248581" TEXT="Lieferantenbetrug">
-<node CREATED="1701722730126" ID="ID_479828706" MODIFIED="1701722737796" TEXT="Manipulation von E-Mails an Finanzabteilungen, um Zahlungen an betr&#xfc;gerische Lieferanten zu veranlassen"/>
-</node>
-<node CREATED="1701722290157" ID="ID_320001793" MODIFIED="1701852556178" TEXT="Board-Mitglieder-Angriffe">
-<node CREATED="1701722750382" ID="ID_1098126173" MODIFIED="1701722752018" TEXT="Gezielte Phishing-Angriffe auf Vorstandsmitglieder, um sensible Unternehmensinformationen zu erhalten"/>
+<node CREATED="1701721468295" ID="ID_178229246" MODIFIED="1702475982377" STYLE="bubble" TEXT="Whaling &amp; Nudging">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1701721767632" ID="ID_1714218005" MODIFIED="1702475964704" POSITION="right" STYLE="bubble" TEXT="Whaling">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1701721780237" ID="ID_102587688" MODIFIED="1702475964704" STYLE="bubble" TEXT="CEO Fraud">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1701722695775" ID="ID_414574149" MODIFIED="1702475964704" STYLE="bubble" TEXT="gef&#xe4;lschte E-Mails, die vorgeben, vom CEO zu stammen und Finanztransaktionen anfordern">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
-<node CREATED="1701721813579" ID="ID_1863309407" MODIFIED="1701722639927" POSITION="left" TEXT="Nudging">
-<node CREATED="1701721835293" ID="ID_1001477306" MODIFIED="1701722641646" TEXT="positive">
-<node CREATED="1701721855810" ID="ID_1320446596" MODIFIED="1701723246560" TEXT="Gesundheits-Apps">
-<node CREATED="1701721871608" ID="ID_1729581520" MODIFIED="1701721887017" TEXT="Anzeige von gesunden Verhaltensweisen oder Ern&#xe4;hrungstipps, um Benutzer zu ges&#xfc;nderen Entscheidungen zu beeinflussen"/>
-</node>
-<node CREATED="1701722488750" ID="ID_451287359" MODIFIED="1701723246016" TEXT="Umweltbewusstsein">
-<node CREATED="1701722506073" ID="ID_1403077683" MODIFIED="1701722508260" TEXT="Hervorhebung umweltfreundlicher Optionen beim Online-Shopping, um nachhaltige Entscheidungen zu f&#xf6;rdern"/>
-</node>
-<node CREATED="1701722520060" ID="ID_1591485737" MODIFIED="1701723245595" TEXT="Energieeinsparung">
-<node CREATED="1701722528687" ID="ID_345307349" MODIFIED="1701722530717" TEXT="Anzeige von Vergleichsdaten im Haushalt, um Benutzer zum Energiesparen zu motivieren"/>
+<node CREATED="1701721789257" ID="ID_1186958709" MODIFIED="1702475964704" STYLE="bubble" TEXT="Identit&#xe4;tsdiebstahl">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1701722720824" ID="ID_167589491" MODIFIED="1702475964703" STYLE="bubble" TEXT="T&#xe4;uschung von F&#xfc;hrungskr&#xe4;ften zur Offenlegung pers&#xf6;nlicher Informationen oder Anmeldeinformationen">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
-<node CREATED="1701721849112" ID="ID_663585185" MODIFIED="1701722656326" TEXT="negative">
-<node CREATED="1701721917439" ID="ID_1385903763" MODIFIED="1701723245147" TEXT="Online-Shopping">
-<node CREATED="1701721923757" ID="ID_610978177" MODIFIED="1701721928875" TEXT="Countdown-Timer oder begrenzte Verf&#xfc;gbarkeit, um Impulsk&#xe4;ufe zu f&#xf6;rdern."/>
+<node CREATED="1701721801478" ID="ID_1201699457" MODIFIED="1702475964703" STYLE="bubble" TEXT="Lieferantenbetrug">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1701722730126" ID="ID_479828706" MODIFIED="1702475964702" STYLE="bubble" TEXT="Manipulation von E-Mails an Finanzabteilungen, um Zahlungen an betr&#xfc;gerische Lieferanten zu veranlassen">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
-<node CREATED="1701721942435" ID="ID_1214533661" MODIFIED="1701723244201" TEXT="Social Media">
-<node CREATED="1701721961234" ID="ID_196557421" MODIFIED="1701721964632" TEXT="Benachrichtigungen &#xfc;ber Aktivit&#xe4;ten von Freunden oder Belohnungen f&#xfc;r regelm&#xe4;&#xdf;ige Nutzung, um die App-Nutzung zu steigern."/>
+<node CREATED="1701722290157" ID="ID_320001793" MODIFIED="1702475964702" STYLE="bubble" TEXT="Board-Mitglieder-Angriffe">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1701722750382" ID="ID_1098126173" MODIFIED="1702475964702" STYLE="bubble" TEXT="Gezielte Phishing-Angriffe auf Vorstandsmitglieder, um sensible Unternehmensinformationen zu erhalten">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
-<node CREATED="1701722597890" ID="ID_1341500935" MODIFIED="1701723243699" TEXT="Cookie-Hinweise">
-<node CREATED="1701722609410" ID="ID_31481988" MODIFIED="1701722616888" TEXT="Pop-up-Benachrichtigungen, die die Zustimmung zur Verwendung von Cookies f&#xf6;rdern"/>
 </node>
-<node CREATED="1701722664732" ID="ID_1603034805" MODIFIED="1701723243209" TEXT="App-Entwicklung">
-<node CREATED="1701722675843" ID="ID_538152849" MODIFIED="1701722677392" TEXT="Nutzung von Gamification-Elementen, um Benutzer zur regelm&#xe4;&#xdf;igen Nutzung von Apps zu motivieren"/>
+</node>
+<node CREATED="1701721813579" ID="ID_1863309407" MODIFIED="1702475964702" POSITION="left" STYLE="bubble" TEXT="Nudging">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1701721835293" ID="ID_1001477306" MODIFIED="1702475964702" STYLE="bubble" TEXT="positive">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1701721855810" ID="ID_1320446596" MODIFIED="1702475964702" STYLE="bubble" TEXT="Gesundheits-Apps">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#000000" CREATED="1701721871608" ID="ID_1729581520" MODIFIED="1702475964701" STYLE="bubble" TEXT="Anzeige von gesunden Verhaltensweisen oder Ern&#xe4;hrungstipps, um Benutzer zu ges&#xfc;nderen Entscheidungen zu beeinflussen">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1701722488750" ID="ID_451287359" MODIFIED="1702475964700" STYLE="bubble" TEXT="Umweltbewusstsein">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1701722506073" ID="ID_1403077683" MODIFIED="1702475964700" STYLE="bubble" TEXT="Hervorhebung umweltfreundlicher Optionen beim Online-Shopping, um nachhaltige Entscheidungen zu f&#xf6;rdern">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1701722520060" ID="ID_1591485737" MODIFIED="1702475964698" STYLE="bubble" TEXT="Energieeinsparung">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1701722528687" ID="ID_345307349" MODIFIED="1702475964698" STYLE="bubble" TEXT="Anzeige von Vergleichsdaten im Haushalt, um Benutzer zum Energiesparen zu motivieren">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node CREATED="1701721849112" ID="ID_663585185" MODIFIED="1702475964698" STYLE="bubble" TEXT="negative">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1701721917439" ID="ID_1385903763" MODIFIED="1702475964698" STYLE="bubble" TEXT="Online-Shopping">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1701721923757" ID="ID_610978177" MODIFIED="1702475964698" STYLE="bubble" TEXT="Countdown-Timer oder begrenzte Verf&#xfc;gbarkeit, um Impulsk&#xe4;ufe zu f&#xf6;rdern.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node CREATED="1701721942435" ID="ID_1214533661" MODIFIED="1702475964698" STYLE="bubble" TEXT="Social Media">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1701721961234" ID="ID_196557421" MODIFIED="1702475964698" STYLE="bubble" TEXT="Benachrichtigungen &#xfc;ber Aktivit&#xe4;ten von Freunden oder Belohnungen f&#xfc;r regelm&#xe4;&#xdf;ige Nutzung, um die App-Nutzung zu steigern.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1701722597890" ID="ID_1341500935" MODIFIED="1702475964698" STYLE="bubble" TEXT="Cookie-Hinweise">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1701722609410" ID="ID_31481988" MODIFIED="1702475964698" STYLE="bubble" TEXT="Pop-up-Benachrichtigungen, die die Zustimmung zur Verwendung von Cookies f&#xf6;rdern">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1701722664732" ID="ID_1603034805" MODIFIED="1702475964698" STYLE="bubble" TEXT="App-Entwicklung">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1701722675843" ID="ID_538152849" MODIFIED="1702475964696" STYLE="bubble" TEXT="Nutzung von Gamification-Elementen, um Benutzer zur regelm&#xe4;&#xdf;igen Nutzung von Apps zu motivieren">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
 </node>
 </node>
 </node>
---
+++ Studium notizen/Welche Auswirkungen hat die Integration von nudgingbasierten Anreizsystemen auf das Spielerlebnis in Free-to-Play-Spielen.md
@@ -0,0 +1,6 @@
+[[Welche Anreizsysteme gibt es]]
+	->Welche Arten von Anreizen können gesetzt werden
+[[Whaling]]
+	https://www.youtube.com/watch?v=xNjI03CGkb4
+	
+[[Nudging]]
